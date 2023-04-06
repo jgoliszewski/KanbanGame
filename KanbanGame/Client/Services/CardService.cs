7a2b551aa43042e1264b39855f204c3450cf4df9
jgoliszewski <jakub.goliszewski01@gmail.com>
2023-04-06 20:02:51 +0200

    Add: stacked columns headers

--- KanbanGame/Client/Services/CardService.cs
+++ KanbanGame/Client/Services/CardService.cs
@@ -49,7 +49,7 @@ public class CardService : ICardService
         {
             Id = lastId++,
             Employee = employee,
-            Column = "Doing"
+            Column = "Delivered"
         };
         return card;
     }
