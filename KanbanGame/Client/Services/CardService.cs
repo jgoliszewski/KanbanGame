a8a20e507ddbbaaf79ff5075fba99df522591143
jgoliszewski <jakub.goliszewski01@gmail.com>
2023-04-16 15:09:23 +0200

    add: displaying cards based on Assignee

--- KanbanGame/Client/Services/CardService.cs
+++ KanbanGame/Client/Services/CardService.cs
@@ -16,40 +16,63 @@ public class CardService : ICardService
         _employeeService = employeeService;
     }
     public List<Card> Cards { get; set; }
-
+    public Dictionary<string, int> ColumnCount;
     public async Task GetCards()
     {
         lastId = 0;
         Cards = new List<Card>();
+        ColumnCount = new Dictionary<string, int>{
+            {"Backlog", 0},
+            {"Analysis", 0},
+            {"DevelopmentWaiting", 0},
+            {"DevelopmentDoing", 0},
+            {"TestWaiting", 0},
+            {"TestDoing", 0},
+            {"Delivered", 0},
+        };
+
         await _kanbanTaskService.GetKanbanTasks();
+        await _employeeService.GetEmployees();
+
         foreach (var t in _kanbanTaskService.KanbanTasks)
         {
-            Cards.Add(TaskToCard(t));
+            if (t.Employee is not null)
+            {
+                Cards.Add(EmployeeToCard(t.Employee, ColumnCount[t.Employee.CurrentRoleString]++));
+                t.StatusString = t.Employee.CurrentRoleString; //todo: make it has more sense
+                Cards.Add(TaskToCard(t, ColumnCount[t.Employee.CurrentRoleString]++));
+                _employeeService.Employees.RemoveAll(e => e.Id == t.Employee.Id);
+            }
+            else
+            {
+                Cards.Add(TaskToCard(t, ColumnCount[t.StatusString]++));
+            }
         }
-        await _employeeService.GetEmployees();
         foreach (var e in _employeeService.Employees)
         {
-            Cards.Add(EmployeeToCard(e));
+            Cards.Add(EmployeeToCard(e, ColumnCount[e.CurrentRoleString]++));
         }
     }
 
 
-    private Card TaskToCard(KanbanTask kanbanTask)
+    private Card TaskToCard(KanbanTask kanbanTask, int rankId)
     {
         var card = new Card()
         {
             Id = lastId++,
+            RankId = rankId,
             KanbanTask = kanbanTask,
             Column = kanbanTask.StatusString
         };
         return card;
     }
 
-    private Card EmployeeToCard(Employee employee)
+    private Card EmployeeToCard(Employee employee, int rankId)
     {
         var card = new Card()
         {
             Id = lastId++,
+            RankId = rankId,
             Employee = employee,
             Column = employee.CurrentRoleString
         };
--- KanbanGame/Shared/Entities/Card.cs
+++ KanbanGame/Shared/Entities/Card.cs
@@ -4,7 +4,7 @@ public class Card
 {
     public int Id { get; set; }
     public string Column { get; set; }
-    public int? InColumnId { get; set; }
+    public int? RankId { get; set; }
     public KanbanTask? KanbanTask { get; set; }
     public Employee? Employee { get; set; }
 }
