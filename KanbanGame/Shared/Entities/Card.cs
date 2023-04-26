aacf5bf534581211aeb88be002dabea7adc7a44b
jgoliszewski <jakub.goliszewski01@gmail.com>
2023-04-26 13:16:42 +0200

    edit: Card Column Getter/Setter

--- KanbanGame/Shared/Entities/Card.cs
+++ KanbanGame/Shared/Entities/Card.cs
@@ -3,8 +3,33 @@ namespace KanbanGame.Shared;
 public class Card
 {
     public int Id { get; set; }
-    public string Column { get; set; }
     public int? RankId { get; set; }
     public KanbanTask? KanbanTask { get; set; }
     public Employee? Employee { get; set; }
+    public string Column 
+    { 
+        get
+        {
+            if (Employee is not null && KanbanTask is null)
+            {
+                return Employee.SF_Column;
+            }
+            else if (KanbanTask is not null && Employee is null)
+            {
+                return KanbanTask.SF_Column;
+            }
+            return "";
+        }
+        set
+        {
+            if (Employee is not null && KanbanTask is null)
+            {
+                Employee.SF_Column = value;
+            }
+            else if (KanbanTask is not null && Employee is null)
+            {
+                KanbanTask.SF_Column = value;
+            }
+        } 
+    }
 }
